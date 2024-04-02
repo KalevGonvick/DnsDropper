b3a92ea2b85fb9b465adab8ee2cd8e86aa939cba
Kalev Gonvick <kalev.gonvick@outlook.com>
2024-04-01 18:09:56 -0700

    Refactored code to be more testable

--- src/filter.rs
+++ src/filter.rs
@@ -2,9 +2,133 @@ use std::borrow::Cow;
 use std::collections::HashSet;
 use std::fs;
 use std::io::{BufReader, Error, ErrorKind, Read, Result};
+use std::ops::Deref;
 use log::{error, trace};
 use reqwest::StatusCode;
 use url::Url;
+
+
+pub(crate) struct FilterList {
+    block_list_sources: Vec<String>
+}
+
+impl FilterList {
+    pub fn new(block_list: &[Cow<'_, str>]) -> FilterList {
+        let mut real_list: Vec<String>  = Vec::new();
+        for borrowed_str in block_list {
+            real_list.push(match borrowed_str {
+                Cow::Borrowed(s) => {
+                    s.to_owned().to_string()
+                }
+                Cow::Owned(s) => {
+                    s.to_string()
+                }
+            });
+        }
+        FilterList {
+            block_list_sources: real_list
+        }
+    }
+
+    pub async fn resolved_block_list(
+        &self
+    ) -> HashSet<Filter> {
+        let mut complete_block_list: HashSet<Filter> = HashSet::new();
+        for source in &self.block_list_sources {
+            if let Ok(url) = Url::parse(source.as_str()) {
+                if url.scheme().eq("file") {
+                    if let Ok(content) = fs::read_to_string(source) {
+                        self.parse_block_list_content(&mut complete_block_list, content).unwrap();
+                    }
+                } else if url.scheme().eq("http") || url.scheme().eq("https") {
+                    self.handle_url_scheme(source.clone(), &mut complete_block_list).await;
+                }
+
+            } else {
+                trace!("Provided string '{}' is not a URL, trying as an external file.", source);
+                self.handle_file_scheme(source.clone(), &mut complete_block_list).await;
+            };
+        }
+
+        return complete_block_list;
+    }
+
+    async fn handle_file_scheme(
+        &self,
+        source: String,
+        complete_block_list: &mut HashSet<Filter>
+    ) {
+        match fs::File::open(source.clone()) {
+            Ok(file) => {
+                let mut buf_reader = BufReader::new(file);
+                let mut body = String::new();
+                match buf_reader.read_to_string(&mut body) {
+                    Ok(_) => {
+                        self.parse_block_list_content(complete_block_list, body).unwrap();
+                    }
+                    Err(err) => {
+                        error!("Error occurred while reading file '{}': {}", source, err);
+                    }
+                }
+            }
+            Err(err) => {
+                error!("Error occurred while reading file '{}': {}", source, err);
+            }
+        }
+    }
+
+    async fn handle_url_scheme(
+        &self,
+        source: String,
+        complete_block_list: &mut HashSet<Filter>
+    ) {
+        match reqwest::get(source.clone()).await {
+            Ok(res) => {
+                trace!("Got response from block-list source: {}", source);
+                if res.status() == StatusCode::OK {
+                    if let Ok(body) = res.text().await {
+                        self.parse_block_list_content(complete_block_list, body).unwrap();
+                    }
+                } else {
+                    error!("Error! Response was: {}", res.status());
+                }
+            }
+            Err(..) => {
+                error!("Error occurred while requesting resource from '{}'", source);
+            }
+        };
+    }
+
+    fn parse_block_list_content(
+        &self,
+        complete_block_list: &mut HashSet<Filter>,
+        content: String
+    ) -> Result<()> {
+        let mut filter: Filter;
+        for line in content.lines() {
+            let split_line: Vec<&str> = line.split_whitespace().collect();
+
+            // we expect lines to follow the pattern of <addr>/s<domain>/n
+            // TODO make it more relaxed format wise. regex?
+            if split_line.len() > 1 && split_line.len() < 3 {
+                filter = Filter {
+                    address: match split_line.get(0) {
+                        Some(x) => x,
+                        None => return Err(Error::new(ErrorKind::InvalidInput, "")),
+                    }.to_string(),
+
+                    domain: match split_line.get(1) {
+                        Some(x) => x,
+                        None => return Err(Error::new(ErrorKind::InvalidInput, "")),
+                    }.to_string(),
+                };
+                complete_block_list.insert(filter);
+            }
+        }
+
+        Ok(())
+    }
+}
 
 #[derive(Hash, Eq, PartialEq, Debug)]
 pub(crate) struct Filter {
@@ -28,93 +152,4 @@ pub(crate) fn should_filter(
         }
     }
     return false;
-}
-
-pub(crate) async fn load_filtered_domains(
-    block_list: &[Cow<'_, str>]
-) -> HashSet<Filter> {
-    let mut complete_block_list: HashSet<Filter> = HashSet::new();
-
-    for source in block_list {
-
-        if let Ok(url) = Url::parse(source) {
-
-            if url.scheme().eq("file") {
-
-                if let Ok(content) = fs::read_to_string(source.clone().into_owned()) {
-                    parse_block_list_content(&mut complete_block_list, content).unwrap();
-                }
-
-            } else if url.scheme().eq("http") || url.scheme().eq("https") {
-
-                if let Ok(res) = reqwest::get(source.clone().into_owned()).await {
-                    trace!("Got response from block-list source: {}", source);
-
-                    if res.status() == StatusCode::OK {
-
-                        if let Ok(body) = res.text().await {
-                            parse_block_list_content(&mut complete_block_list, body).unwrap();
-                        }
-
-                    } else {
-                        error!("Error! Response was: {}", res.status());
-                    }
-
-                } else {
-                    error!("Error occurred while requesting resource from '{}'", source);
-                };
-            }
-
-        } else {
-            trace!("Provided string '{}' is not a URL, trying as an external file.", source);
-
-            if let Ok(file) = fs::File::open(source.clone().into_owned()) {
-                let mut buf_reader = BufReader::new(file);
-                let mut body = String::new();
-
-                if let Ok(_) = buf_reader.read_to_string(&mut body) {
-                    parse_block_list_content(&mut complete_block_list, body).unwrap();
-
-                } else if let Err(err) = buf_reader.read_to_string(&mut body) {
-                    error!("Error occurred while reading file '{}': {}", source, err);
-                }
-
-            } else if let Err(err) = fs::File::open(source.clone().into_owned()) {
-                error!("Error occurred while reading file '{}': {}", source, err);
-            }
-        };
-    }
-
-    return complete_block_list;
-}
-
-fn parse_block_list_content(
-    complete_block_list: &mut HashSet<Filter>,
-    content: String
-) -> Result<()> {
-
-    let mut filter: Filter;
-
-    for line in content.lines() {
-        let split_line: Vec<&str> = line.split_whitespace().collect();
-
-        // we expect lines to follow the pattern of <addr>/s<domain>/n
-        // TODO make it more relaxed format wise. regex?
-        if split_line.len() > 1 && split_line.len() < 3 {
-            filter = Filter {
-                address: match split_line.get(0) {
-                    Some(x) => x,
-                    None => return Err(Error::new(ErrorKind::InvalidInput, "")),
-                }.to_string(),
-
-                domain: match split_line.get(1) {
-                    Some(x) => x,
-                    None => return Err(Error::new(ErrorKind::InvalidInput, "")),
-                }.to_string(),
-            };
-            complete_block_list.insert(filter);
-        }
-    }
-
-    Ok(())
 }
--- src/server.rs
+++ src/server.rs
@@ -8,6 +8,7 @@ mod internal;
 use std::borrow::Cow;
 use std::collections::{HashSet};
 use std::io::{Error, ErrorKind};
+use std::net::SocketAddr;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::Duration;
@@ -15,11 +16,12 @@ use tokio::io::Result;
 use tokio::net::UdpSocket;
 use tokio::time::timeout;
 use clap::Parser;
+use tokio::runtime::Runtime;
 
-use crate::filter::Filter;
+use crate::filter::{Filter, FilterList};
 use crate::logging::WARN_STYLE;
 use crate::logging::ERROR_STYLE;
-use crate::dns::{BytePacketBuffer, DnsPacket, ResultCode};
+use crate::dns::{BytePacketBuffer, DnsPacket, DnsQuestion, ResultCode};
 use crate::internal::INTERNAL_CONFIG;
 use crate::server_config::ServerConfig;
 
@@ -36,8 +38,8 @@ fn main() -> Result<()> {
         Ok(server_config) => {
             logging::setup(server_config.logging.level.as_ref());
             logging::print_title();
-
-            start_server(server_config)
+            let server: Server = Server::new(server_config);
+            server.start()
         }
 
         Err(_) => {
@@ -46,175 +48,233 @@ fn main() -> Result<()> {
     }
 }
 
-
-fn start_server<'a>(
-    config: Cow<ServerConfig>
-) -> Result<()> {
-
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .worker_threads(config.server.worker_thread_count as usize)
-        .thread_name_fn(|| {
-            static ATOMIC_ID: AtomicUsize = AtomicUsize::new(0);
-            let id = ATOMIC_ID.fetch_add(1, Ordering::SeqCst);
-            format!("{}-{}", INTERNAL_CONFIG.worker_thread_name, id)
-        })
-        .on_thread_start(|| {
-            log::debug!("Starting worker thread.");
-        })
-        .on_thread_stop(|| {
-            log::debug!("Stopping worker thread.");
-        })
-        .enable_io()
-        .enable_time()
-        .build()?;
-
-    rt.block_on(async {
-        let complete_block_list = filter::load_filtered_domains(config.clone().udp_proxy.domain_block_lists.as_ref()).await;
-        let bind = config.udp_proxy.bind.to_string();
-
-        let socket = UdpSocket::bind(bind).await?;
-        let arc_socket = Arc::new(socket);
-        let arc_config = Arc::new(config.clone().into_owned());
-
-        log::debug!("Block list contains '{}' different domain names.", complete_block_list.len());
-
-        let arc_block_list = Arc::new(complete_block_list);
-        log::info!("Started DNS Proxy: {}", config.udp_proxy.bind);
-        loop {
-            match arc_socket.ready(tokio::io::Interest::READABLE).await {
-                Ok(r) => {
-                    if r.is_readable() {
-
-                        /* shared constants for threads */
-                        let arc_socket_clone = arc_socket.clone();
-                        let arc_config_clone = arc_config.clone();
-                        let arc_block_list_clone = arc_block_list.clone();
-
-                        tokio::spawn(async move {
-                            start_udp_dns_listener(arc_socket_clone, arc_config_clone, arc_block_list_clone).await;
-                        });
-                    }
-                }
-
-                Err(err) => {
-                    log::error!("Error trying to read from socket: {}", err);
-                }
-            }
-        }
-    })
+struct Server {
+    runtime: Runtime,
+    server_config: Arc<ServerConfig>
 }
 
-async fn start_udp_dns_listener(
+impl Server {
+    pub fn new(
+        config: Cow<ServerConfig>
+    ) -> Server {
+        let arc_config = Arc::new(config.clone().into_owned());
+        let rt = tokio::runtime::Builder::new_multi_thread()
+            .worker_threads(config.server.worker_thread_count as usize)
+            .thread_name_fn(|| {
+                static ATOMIC_ID: AtomicUsize = AtomicUsize::new(0);
+                let id = ATOMIC_ID.fetch_add(1, Ordering::SeqCst);
+                format!("{}-{}", INTERNAL_CONFIG.worker_thread_name, id)
+            })
+            .on_thread_start(|| {
+                log::debug!("Starting worker thread.");
+            })
+            .on_thread_stop(|| {
+                log::debug!("Stopping worker thread.");
+            })
+            .enable_io()
+            .enable_time()
+            .build();
+
+        Server {
+            server_config: arc_config,
+            runtime: rt.unwrap()
+        }
+    }
+
+    pub fn start(
+        &self
+    ) -> Result<()> {
+
+        self.runtime.block_on(async {
+
+            let filter_list: FilterList = FilterList::new(self.server_config.clone().udp_proxy.domain_block_lists.as_ref());
+            let complete_block_list = filter_list.resolved_block_list().await;
+            let bind = self.server_config.udp_proxy.bind.to_string();
+
+            let socket = UdpSocket::bind(bind).await?;
+            let arc_socket = Arc::new(socket);
+
+            log::debug!("Block list contains '{}' different domain names.", complete_block_list.len());
+
+            let arc_block_list = Arc::new(complete_block_list);
+            log::info!("Started DNS Proxy: {}", self.server_config.udp_proxy.bind);
+            loop {
+                match arc_socket.ready(tokio::io::Interest::READABLE).await {
+                    Ok(r) => {
+                        if r.is_readable() {
+
+                            /* shared constants for threads */
+                            let arc_socket_clone = arc_socket.clone();
+                            let arc_config_clone = self.server_config.clone();
+                            let arc_block_list_clone = arc_block_list.clone();
+
+                            tokio::spawn(async move {
+                                let server_thread = ServerThread::new(
+                                    arc_socket_clone,
+                                    arc_config_clone,
+                                    arc_block_list_clone
+                                );
+                                server_thread.start().await
+                            });
+                        }
+                    }
+
+                    Err(err) => {
+                        log::error!("Error trying to read from socket: {}", err);
+                    }
+                }
+            }
+        })
+    }
+}
+
+struct ServerThread {
     socket: Arc<UdpSocket>,
     server_config: Arc<ServerConfig>,
     block_list: Arc<HashSet<Filter>>
-) {
-    let mut req = BytePacketBuffer::new();
-    let (len, src) = match socket.try_recv_from(&mut req.buf) {
-        Ok(r) => {
-            log::trace!("DNS Listener received data of length: {} bytes", r.0);
-            r
-        },
+}
 
-        Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
-            return;
+impl ServerThread {
+    pub fn new(
+        socket: Arc<UdpSocket>,
+        server_config: Arc<ServerConfig>,
+        block_list: Arc<HashSet<Filter>>
+    ) -> ServerThread {
+        ServerThread {
+            socket,
+            server_config,
+            block_list
         }
+    }
 
-        /* Lots of traffic produced when using loopback configuration on Windows. Why? I have no idea and I don't look forward to knowing. */
-        Err(ref e) if e.kind() == ErrorKind::ConnectionReset => {
-            log::trace!("Connection reset error occurred for local socket '{}'.", socket.local_addr().unwrap());
-            return;
-        }
+    pub async fn start(
+        &self
+    ) {
 
-        Err(err) => {
-            log::error!("Failed to receive message from configured bind: {}", err);
-            return;
-        }
-    };
+        let mut req = BytePacketBuffer::new();
+        let (len, src) = match self.socket.try_recv_from(&mut req.buf) {
 
-    if let Ok(mut packet) = DnsPacket::from_buffer(&mut req) {
-        log::trace!("Successfully constructed a DNS packet from received data.");
+            Ok(r) => {
+                log::trace!("DNS Listener received data of length: {} bytes", r.0);
+                r
+            },
 
-        if let None = packet.questions.get(0) {
-            log::error!("DNS Packet contains no questions");
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                return;
+            }
 
-        } else if let Some(query) = packet.questions.get(0) {
-            log::trace!("Checking to see if the domain '{}' and/or record type '{}' should be filtered.", query.name, query.record_type.to_num());
+            /* Lots of traffic produced when using loopback configuration on Windows. Why? I have no idea and I don't look forward to knowing. */
+            Err(ref e) if e.kind() == ErrorKind::ConnectionReset => {
+                log::trace!("Connection reset error occurred for local socket '{}'.", self.socket.local_addr().unwrap());
+                return;
+            }
 
-            if filter::should_filter(&query.name, &block_list) {
-                let mut response_buffer = BytePacketBuffer::new();
-                log::info!("{ERROR_STYLE}!BLOCK!{ERROR_STYLE:#} -- {WARN_STYLE}{}:{}{WARN_STYLE:#}", query.name, query.record_type.to_num());
-                packet.header.result_code = ResultCode::NXDOMAIN;
+            Err(err) => {
+                log::error!("Failed to receive message from configured bind: {}", err);
+                return;
+            }
+        };
 
-                if let Ok(_) = packet.write(&mut response_buffer) {
+        if let Ok(mut packet) = DnsPacket::from_buffer(&mut req) {
+            log::trace!("Successfully constructed a DNS packet from received data.");
+            match packet.questions.get(0) {
 
-                    if let Ok(data) = response_buffer.get_range(0, response_buffer.pos) {
-
-                        if let Err(err) = socket.send_to(&data, &src).await {
-                            log::error!("Reply to '{}' failed {:?}", &src, err);
-                        }
-
-                    } else if let Err(err) = response_buffer.get_range(0, response_buffer.pos) {
-                        log::error!("Could not retrieve buffer range: {}", err);
-                    }
-
-                } else if let Err(err) = packet.write(&mut response_buffer) {
-                    log::error!("Error writing packet: {}", err);
+                None => {
+                    log::error!("DNS Packet contains no questions.");
                 }
 
-            } else {
+                Some(query) => {
+                    log::trace!("Checking to see if the domain '{}' and/or record type '{}' should be filtered.", query.name, query.record_type.to_num());
 
-                for host in server_config.udp_proxy.dns_hosts.as_ref() {
+                    // TODO - maybe introduce handlers?
+                    if filter::should_filter(&query.name, &self.block_list) {
+                        let mut response_buffer = BytePacketBuffer::new();
+                        log::info!("{ERROR_STYLE}!BLOCK!{ERROR_STYLE:#} -- {WARN_STYLE}{}:{}{WARN_STYLE:#}", query.name, query.record_type.to_num());
+                        packet.header.result_code = ResultCode::NXDOMAIN;
 
-                    if let Ok(data) = do_lookup(&req.buf[..len], host.to_string(), server_config.udp_proxy.timeout).await {
-
-                        if let Err(err) = socket.send_to(&data, &src).await {
-                            log::error!("Replying to '{}' failed {:?}", &src, err);
-                            continue;
+                        match packet.write(&mut response_buffer) {
+                            Ok(_) => {
+                                match response_buffer.get_range(0, response_buffer.pos) {
+                                    Ok(data) => {
+                                        if let Err(err) = self.socket.send_to(&data, &src).await {
+                                            log::error!("Reply to '{}' failed {:?}.", &src, err);
+                                        }
+                                    }
+                                    Err(err) => {
+                                        log::error!("Could not retrieve buffer range: {}.", err);
+                                    }
+                                }
+                            }
+                            Err(err) => {
+                                log::error!("Error writing packet: {}.", err);
+                            }
                         }
 
-                        log::debug!("Forwarded the answer for the domain '{}' from '{}'.", query.name, host);
-                        return;
-
-                    } else if let Err(err) = do_lookup(&req.buf[..len], host.to_string(), server_config.udp_proxy.timeout).await {
-                        log::error!("Error processing request: {:?}", err);
-                        return;
-
                     } else {
-                        unreachable!()
-                    };
+                        self.handle_proxy(req, len, src, query).await
+                    }
                 }
             }
         }
     }
-}
 
-async fn do_lookup(buf: &[u8], remote_host: String, connection_timeout: i64) -> Result<Vec<u8>> {
-    let duration = Duration::from_millis(connection_timeout as u64);
-    let socket = UdpSocket::bind(("0.0.0.0", 0)).await?;
+    fn handle_filtered_response() {
 
-    log::debug!("Outbound UDP socket bound to port '{}' for the host destination '{}'.", socket.local_addr().unwrap().port(), remote_host);
+    }
 
-    let data: Result<Vec<u8>> = timeout(duration, async {
-        socket.send_to(buf, remote_host.to_string()).await?;
+    async fn handle_proxy(
+        &self,
+        req: BytePacketBuffer,
+        len: usize,
+        src: SocketAddr,
+        query: &DnsQuestion
+    ) {
+        for host in self.server_config.udp_proxy.dns_hosts.as_ref() {
 
-        let mut response = [0; INTERNAL_CONFIG.max_udp_packet_size as usize];
-        let length = socket.recv(&mut response).await?;
+            match self.do_proxy(&req.buf[..len], host.to_string()).await {
+                Ok(data) => {
+                    if let Err(err) = self.socket.send_to(&data, &src).await {
+                        log::error!("Replying to '{}' failed {:?}.", &src, err);
+                        continue;
+                    }
+                    log::debug!("Forwarded the answer for the domain '{}' from '{}'.", query.name, host);
+                    return;
+                }
+                Err(err) => {
+                    log::error!("Error processing request: {:?}.", err);
+                    return;
+                }
+            }
+        }
+    }
 
-        log::debug!("Received response from '{}' for port '{}' with a length of: {} bytes", remote_host, socket.local_addr().unwrap().port(), length);
+    async fn do_proxy(
+        &self,
+        buf: &[u8],
+        remote_host: String
+    ) -> Result<Vec<u8>> {
+        let duration = Duration::from_millis(self.server_config.udp_proxy.timeout as u64);
+        let socket = UdpSocket::bind(("0.0.0.0", 0)).await?;
 
-        Ok(response[..length].to_vec())
-    }).await?;
+        log::debug!("Outbound UDP socket bound to port '{}' for the host destination '{}'.", socket.local_addr().unwrap().port(), remote_host);
 
-    if let Ok(data) = data {
-        return Ok(data)
+        let data: Result<Vec<u8>> = timeout(duration, async {
+            socket.send_to(buf, remote_host.to_string()).await?;
+            let mut response = [0; INTERNAL_CONFIG.max_udp_packet_size as usize];
+            let length = socket.recv(&mut response).await?;
+            log::debug!("Received response from '{}' for port '{}' with a length of: {} bytes", remote_host, socket.local_addr().unwrap().port(), length);
+            Ok(response[..length].to_vec())
+        }).await?;
 
-    } else if let Err(err) = data {
-        log::error!("Agent request to {:?} {:?}", remote_host, err);
-        Err(err)
-
-    } else {
-        unreachable!()
+        match data {
+            Ok(data) => {
+                return Ok(data)
+            }
+            Err(err) => {
+                log::error!("Agent request to {:?} {:?}", remote_host, err);
+                Err(err)
+            }
+        }
     }
 }
 
